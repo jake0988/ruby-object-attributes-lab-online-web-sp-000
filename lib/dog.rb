3d6b51164ebfb2a6d96529118e4ad1432d652f6c
Jake G <jake098877@gmail.com>
2020-05-03 00:48:59 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dog_name)
+    @newdog_name = dog_name
+  end
+
+  def name
+    @newdog_name
+  end
+
+  def breed=(breed_name)
+    @newbreed = breed_name
+  end
+
+  def breed
+    @newbreed
+  end
+end
